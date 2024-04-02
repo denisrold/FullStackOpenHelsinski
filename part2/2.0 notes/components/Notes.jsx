c460fa54151df3c76d32f0879923bbeb2d0f3db5
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2024-04-02 14:32:44 -0300

    Server comunications notes

--- part2/2.0 notes/components/Notes.jsx
+++ part2/2.0 notes/components/Notes.jsx
@@ -1,2 +1,16 @@
-const Note = ({content})=> <li>{content}</li>
+const Note = ({content,important,toggleImportance,id })=>{
+   const handleImportance=()=>{
+   
+        toggleImportance(id);
+    }
+    const label = important
+    ? 'make not important' : 'make important'
+    return (
+        <>
+        <li>{content}</li>
+        <button onClick={handleImportance }>{label}</button>
+        </>
+        )
+    }
+    
 export default Note
--- part2/2.0 notes/src/App.jsx
+++ part2/2.0 notes/src/App.jsx
@@ -1,21 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Note from "../components/Notes";
 import Form from "../components/Form";
 import Button from "../components/Button";
+import axios from 'axios';
 
 const App = ({ notes }) => {
-  const [notesArray,setNotesArray] = useState(notes);
+  const [notesArray,setNotesArray] = useState([]);
   const [newNote,setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
 
+  useEffect(()=>{
+   !notesArray.length ? axios.get("http://localhost:3001/notes")
+  .then(response=>setNotesArray([...response.data])) : ""}
+  ,[]);
+
   const addNote = (event) => {
     event.preventDefault();
+    let newID= (notesArray.length +1).toString();
     const newObject = {
-      id: notesArray.length +1 ,
+      id:newID,
       content: newNote,
       important: Math.random() > 0.5
     }
     setNotesArray([...notesArray,newObject]);
+
+    axios
+    .post('http://localhost:3001/notes', newObject)
+    .then(response => {
+      console.log(response)
+    })
     setNewNote('');
   }
 
@@ -25,12 +38,23 @@ const App = ({ notes }) => {
 
   const notesToShow = showAll? notesArray : notesArray.filter(note => note.important === true)
 
+
+  //Importance Change
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notesArray.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+    axios.put(url, changedNote).then(response => {
+      setNotesArray(notesArray.map(note => note.id !== id ? note : response.data))
+    })
+  }
+
   return (
     <div>
       <h1>Notes</h1>
       <Button setShowAll={setShowAll} showAll={showAll}/>
       <ul>
-      {notesToShow.map((note)=><Note key={note.id} content={note.content}/>)} 
+      {notesToShow.map((note)=><Note key={note.id} id={note.id} content={note.content} important={note.important} toggleImportance={toggleImportanceOf}/>)} 
       </ul>
       <Form addNote={addNote} handleNoteChange={handleNoteChange} value={newNote} /> 
     </div>
