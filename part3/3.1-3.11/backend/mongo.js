40729372bf3d85fefe673b983c292c10594243cb
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2024-04-21 07:50:20 -0300

    database configuration

--- part3/3.1-3.11/backend/mongo.js
+++ part3/3.1-3.11/backend/mongo.js
@@ -1,39 +1,49 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-// if (process.argv.length < 3) {
-//   console.log("give password as argument");
-//   process.exit(1);
-// }
-
-// const password = process.argv[2];
+if (process.argv.length < 4) {
+  console.log("give data name and number as argument");
+  console.log('Example: node mongo.js "Anna heizenberg" "040-1234556"');
+  process.exit(1);
+}
+//mongodb auth requeriments
 const password = process.env.MONGODB_PASSWORD;
 const user = process.env.MONGODB_USER;
+
+//data arguments
+const name = process.argv[2];
+const number = process.argv[3];
+
+//mongodb connection
 const url = `mongodb+srv://${user}:${password}@cluster0.by5femd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
 mongoose.set("strictQuery", false);
-
 mongoose.connect(url);
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+const phoneBookSchema = new mongoose.Schema({
+  name: String,
+  number: String,
 });
 
-const Note = mongoose.model("Note", noteSchema);
+const Contact = mongoose.model("Contact", phoneBookSchema);
 
-// const note1 = new Note({
-//   content: "HTML is easy",
-//   important: true,
-// });
+const contact = new Contact({
+  name: name,
+  number: number,
+});
 
-// note1.save().then((result) => {
-//   console.log("note saved!");
-//   // mongoose.connection.close();
-// });
+contact
+  .save()
+  .then((result) => {
+    console.log({ "contact saved!": contact });
+    mongoose.connection.close();
+  })
+  .catch((err) => console.log(err));
 
-// Note.find({}).then((result) => {
-//   result.forEach((note) => {
-//     console.log(note);
-//   });
-//   mongoose.connection.close();
-// });
+// Contact.find({})
+//   .then((result) => {
+//     result.forEach((note) => {
+//       console.log(note);
+//     });
+//     mongoose.connection.close();
+//   })
+//   .catch((err) => console.log(err));
