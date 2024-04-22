ba2c177606b528e585ff20b06b0d6dacfcd741a9
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2024-04-22 05:59:46 -0300

    connect phonebook/mongodb

--- part3/3.1-3.11/backend/index.js
+++ part3/3.1-3.11/backend/index.js
@@ -3,6 +3,8 @@ const persons = require("./data.js");
 const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 app.use(express.static("dist"));
 app.use(cors());
@@ -18,16 +20,58 @@ const unknowEndPoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint. NOT FOUND" });
 };
 
+//mongodb auth requeriments
+const password = process.env.MONGODB_PASSWORD;
+const user = process.env.MONGODB_USER;
+
+//mongodb connection
+const url = `mongodb+srv://${user}:${password}@cluster0.by5femd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(url)
+  .then((res) => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const phoneBookSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+//format mongo responses
+phoneBookSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    //transform object _id to string and add to the object response.
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const Contact = mongoose.model("Contact", phoneBookSchema);
 //READ
-app.get("/api/info", (req, res) => {
-  const info = persons.length;
+app.get("/api/info", async (req, res) => {
+  const info = await Contact.find({})
+    .then((result) => {
+      return result.length;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+
   const date = new Date();
   res.send(`<h2>Phone has info of ${info} Peoples</h2>
   <h3>${date}</h3>`);
 });
 
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Contact.find({})
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => console.log(err, err.message));
 });
 
 app.get("/api/persons/:id", (req, res) => {
@@ -79,4 +123,18 @@ app.use(unknowEndPoint);
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log("Listen port 3001");
+});
+
+app.on("close", () => {
+  mongoose.connection.close(() => {
+    console.log("MongoDB connection closed due to server shutdown");
+    process.exit(0);
+  });
+});
+
+// Handle Ctrl+C signal to gracefully shutdown the server
+process.on("SIGINT", () => {
+  server.close(() => {
+    console.log("Server closed");
+  });
 });
