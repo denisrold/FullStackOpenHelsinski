b12c563eeb8833782d633c95e145e9c7662ddbd0
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2024-05-01 09:17:00 -0300

    SuperTesting ok

--- part4/4.0/backend/tests/note_api.test.js
+++ part4/4.0/backend/tests/note_api.test.js
@@ -1,27 +1,30 @@
-const { test, after } = require("node:test");
+const { test, after, describe } = require("node:test");
 const assert = require("node:assert");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
 
 const api = supertest(app);
+describe("Testing Api Notes", () => {
+  test("notes are returned as json", async () => {
+    await api
+      .get("/api/notes/")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
 
-test("notes are returned as json", async () => {
-  await api
-    .get("/api/notes")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
-});
+  test("there are two notes", async () => {
+    const response = await api.get("/api/notes");
 
-test("there are two notes", async () => {
-  const response = await api.get("/api/notes");
-  assert.strictEqual(response.body.length, 2);
-});
+    assert.strictEqual(response.body.length, 3);
+  });
 
-test("the first note is about HTTP methods", async () => {
-  const response = await api.get("/api/notes");
-  const contents = response.body.map((e) => e.content);
-  assert.strictEqual(contents.includes("HTML is easy"), true);
+  test("the first note is about HTTP methods", async () => {
+    const response = await api.get("/api/notes");
+    const contents = response.body.map((e) => e.content);
+    //   assert.strictEqual(contents.includes("HTML is easy"), true);
+    assert(contents.includes("HTML is easy"));
+  });
 });
 
 after(async () => {
