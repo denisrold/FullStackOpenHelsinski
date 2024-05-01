15d477c62c39ee5afb311b9f1be80736cd824b3d
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2024-05-01 09:54:39 -0300

    testing bdd

--- part4/4.0/backend/tests/note_api.test.js
+++ part4/4.0/backend/tests/note_api.test.js
@@ -1,12 +1,32 @@
-const { test, after, describe } = require("node:test");
+const { test, after, beforeEach, describe } = require("node:test");
+const Note = require("../models/note");
 const assert = require("node:assert");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
 
+const initialNotes = [
+  {
+    content: "HTML is easy",
+    important: false,
+  },
+  {
+    content: "Browser can execute only JavaScript",
+    important: true,
+  },
+];
+
+beforeEach(async () => {
+  await Note.deleteMany({});
+  let noteObject = new Note(initialNotes[0]);
+  await noteObject.save();
+  noteObject = new Note(initialNotes[1]);
+  await noteObject.save();
+});
+
 const api = supertest(app);
 describe("Testing Api Notes", () => {
-  test("notes are returned as json", async () => {
+  test.only("notes are returned as json", async () => {
     await api
       .get("/api/notes/")
       .expect(200)
@@ -16,7 +36,7 @@ describe("Testing Api Notes", () => {
   test("there are two notes", async () => {
     const response = await api.get("/api/notes");
 
-    assert.strictEqual(response.body.length, 3);
+    assert.strictEqual(response.body.length, initialNotes.length);
   });
 
   test("the first note is about HTTP methods", async () => {
