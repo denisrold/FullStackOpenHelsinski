010924cc51309e15ce50069c60f7d907b18cc360
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2024-05-06 09:02:55 -0300

    middleware corrections

--- part4/4.1-4.12/backend/app.js
+++ part4/4.1-4.12/backend/app.js
@@ -16,10 +16,9 @@ const errorHandler = require("./utils/errorHandler");
 app.use(cors());
 app.use(express.json());
 if (!process.env.NODE_ENV == "test") app.use(requestMorgan());
-app.use(tokenExtractor);
 app.use("/api/login", loginRouter);
-app.use("/api/users", usersRouter);
-app.use("/api/blogs", blogsRouter);
+app.use("/api/users", tokenExtractor, usersRouter);
+app.use("/api/blogs", tokenExtractor, blogsRouter);
 app.use(unknownEndPoint);
 app.use(errorHandler);
 
