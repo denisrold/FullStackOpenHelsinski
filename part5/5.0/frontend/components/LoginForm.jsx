fa880da7b09e208ba059f7e6cc8959f30c9d3faf
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2024-05-09 06:56:52 -0300

    Refactoring user

---
+++ part5/5.0/frontend/components/LoginForm.jsx
@@ -0,0 +1,35 @@
+import loginService from '../src/services/login'
+import {useState} from 'react';
+const LoginForm = ({loginHandle})=>{
+    const [username,setUsername] = useState('');
+    const [password,setPassword] = useState('');
+    const {setUser,setErrorMessage}=loginHandle;
+    //LOGIN
+    const handleLoggin = async  (event)=>{
+        event.preventDefault();
+        try{
+        const userLog = await loginService.login({username,password});
+        setUser(userLog);
+        setUsername('');
+        setPassword('');
+  
+    } catch(err){
+        setErrorMessage(err.response.data.error.replace(err.response.data.error[0],err.response.data.error[0].toUpperCase()) + '.')
+        setTimeout(() => {
+             setErrorMessage(null)
+        }, 3000)
+  }
+};
+    return(
+    <form className="userForm">
+        <div>
+            <input name="Username" type="text" value={username} onChange={({target})=>setUsername(target.value)} placeholder="Username" />
+        </div>
+        <div>
+            <input name="Password" type="password" value={password} onChange={({target})=>setPassword(target.value)} placeholder="Password" />
+        </div>
+        <button type="submit" onClick={handleLoggin}>Login</button>
+    </form>)
+};
+
+export default LoginForm;
--- part5/5.0/frontend/src/App.jsx
+++ part5/5.0/frontend/src/App.jsx
@@ -5,15 +5,14 @@ import Button from "../components/Button";
 import noteService from './services/notes'
 import Notifications from '../components/Notifications'
 import Footer from '../components/Footer';
-import login from "./services/login";
+import LoginForm from "../components/LoginForm";
 
 const App = () => {
   const [notesArray,setNotesArray] = useState([]);
   const [newNote,setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
-  const [username,setUsername] = useState('');
-  const [password,setPassword] = useState('');
+
   const [user,setUser] = useState(null);
 
    useEffect(()=>{
@@ -21,25 +20,8 @@ const App = () => {
   .then(initialNotes=>setNotesArray(initialNotes)).catch(err=>console.log(err)) : ""
 }
   ,[notesArray]);
-
-
-  //LOGIN
-  const handleLoggin =async  (event)=>{
-    event.preventDefault();
-    try{
-    const userLog = await login({username,password});
-    setUser(userLog);
-    setUsername('');
-    setPassword('');
-    console.log(user);
-  } catch(err){
-    setErrorMessage(err.response.data.error.replace(err.response.data.error[0],err.response.data.error[0].toUpperCase()) + '.')
-    setTimeout(() => {
-      setErrorMessage(null)
-    }, 3000)
-  }
   
-};
+// };
 //handle logout
 const handleLogout=()=>{
   setUser(null);
@@ -71,9 +53,7 @@ const handleLogout=()=>{
       const note = notesArray.find(n => n.id === id)
       const changedNote = { ...note, important: !note.important }
      //update and errors.
-
       noteService.update(id, changedNote).then(updatedNote => {
-
         setNotesArray(notesArray.map(note => note.id !== id ? note : updatedNote))
       }).catch(error => {
         setErrorMessage(`Note '${note.content}' NotFound`);
@@ -86,22 +66,13 @@ const handleLogout=()=>{
 
   //FILTER NOTES
   const notesToShow = showAll? notesArray : notesArray.filter(note => note.important === true)
-
+  
   return (
     <div>
       <h1 id="NOTES">Notes</h1>
       {!user&&
-      <form className="userForm">
-        <div>
-          <input name="Username" type="text" value={username} onChange={(target)=>setUsername(target.target.value)} placeholder="Username" />
-        </div>
-        <div>
-          <input name="Password" type="password" value={password} onChange={(target)=>setPassword(target.target.value)} placeholder="Password" />
-        </div>
-        <button type="submit" onClick={handleLoggin}>Login</button>
-      </form>}
+      <LoginForm  loginHandle={{user,setUser,setErrorMessage}} />}     
       <Notifications message={errorMessage} />
-      
       {user &&
       <>
       <div className="LogoutSesion">
