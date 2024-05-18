269358feb3980656e1d03ac7aea8e56ce7d44ec5
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2024-05-17 22:13:37 -0300

    finished liks

--- part5/5.1/frontend/components/Blog.jsx
+++ part5/5.1/frontend/components/Blog.jsx
@@ -9,10 +9,10 @@ const Blogs = ({blog})=>{
   const [unlikes,setUnlike] =useState(false);
  
   const getUserLike = async ()=>{
-    const userToken = window.localStorage.getItem('userLogged');
-    const JSONPARSE = await JSON.parse(userToken);
+    const getUserToken = window.localStorage.getItem('userLogged');
+    const {token} = await JSON.parse(getUserToken);
     const res = await blogService.getBlogsByID(id);
-    userService.setToken(JSONPARSE.token);
+    userService.setToken(token);
     const userId =await userService.userId();
     const arrayUser = res.likesUserId.find(u=>u == userId);
     if(!arrayUser)setUnlike(false)
@@ -27,9 +27,9 @@ const Blogs = ({blog})=>{
     const handleLikes= async()=>{
     try{
       //USER AND LIKES INFO.
-      const userToken = window.localStorage.getItem('userLogged');
-      const JSONPARSE = await JSON.parse(userToken);
-      blogService.setToken(JSONPARSE.token);
+      const getUserToken = window.localStorage.getItem('userLogged');
+      const {token} = await JSON.parse(getUserToken);
+      blogService.setToken(token);
       //like or unlike user.
       setUnlike(!unlikes);
       const service = await blogService.updateLikes(blog,unlikes);
@@ -40,16 +40,21 @@ const Blogs = ({blog})=>{
      return(
         <section  className='blogContainer'>
              <h4>{title}</h4>
+                <article className='likeContainer'>
+                    <span>
+                    Likes:
+                    <span className={'Liked'}style={{fontWeight:'bolder'}}> {like}</span>
+                  </span>
+                  <button onClick={handleLikes}>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className={`heart ${unlikes?"heartLike":''}`}>
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                  </svg>
+                  </button>
+                </article>
              <Toggable buttonLabel={"show"} buttonlabelCancel={"hide"}>
                 <h5>
                 Author: {author}
                 </h5>
-                <article className='likeContainer'>
-                  <span>
-                  Likes: <span className={`${unlikes&&'Liked'}`}style={{fontWeight:'bolder'}}>{like}</span>
-                  </span>
-                  <button onClick={handleLikes}>like</button>
-                </article>
                 <h5>
                   User: {userId.name}
                 </h5>
---
+++ part5/5.1/frontend/components/Likes.jsx
@@ -0,0 +1,3 @@
+const Likes =()=>{}
+
+export default Likes;
