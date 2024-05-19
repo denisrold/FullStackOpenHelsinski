5658d178522d7c81cd256f99d78b343b3dbc2186
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2024-05-19 19:35:58 -0300

    styles notes

--- part5/5.0/frontend/components/Notes.jsx
+++ part5/5.0/frontend/components/Notes.jsx
@@ -8,13 +8,19 @@ const Note = ({setChangesNotes,content,important,toggleImportance,id })=>{
     const label = important
     ? 'Not important' : 'Make important'
     return (
-        <div className={`noteContent ${important?"important":""}`}>
-        <li className={`note `}>
+        <section className={`noteContent ${important?"important":""}`}>
+        <li className={`note notelength`}>
         {content}
         </li>
+        <article className="noteButtonContainer">
         <DeleteNote  setChangesNotes={setChangesNotes} note={id}/>
-        <button className={`${important?"notimportant":""}`} onClick={handleImportance }>{label}</button>
-        </div>
+        <button className={`importantButton ${important?"notimportant":""}`} onClick={handleImportance }>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="noteIcons">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
+            </svg>
+        </button>
+        </article>
+        </section>
         )
     }
     
