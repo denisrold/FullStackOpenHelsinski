86e3b553919d492ef57a402d333d141d37b911cd
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2024-05-22 15:58:50 -0300

    testing

---
+++ part5/5.0/frontend/test/Note.test.jsx
@@ -0,0 +1,43 @@
+import {render,screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+import NoteContent from'../components/NoteContent';
+import Note from '../components/Notes';
+
+test('Render content',() => {
+    const note = {
+        content: 'Component testing is done with react-testing-library',
+        important: true
+    }
+    render(<NoteContent content={note.content} />)
+    //debugger
+    screen.debug();
+
+    const element = screen.getByText('Component testing is done with react-testing-library')
+
+    screen.debug(element);
+    // expect(element).toBeDefined()
+})
+
+
+test('Render content container',() => {
+    const note = {
+        content: 'Component testing is done with react-testing-library',
+        important: true
+    }
+   const {container} =  render(<NoteContent content={note.content} />)
+   const div = container.querySelector('.note');
+
+   expect(div).toHaveTextContent(
+    'Component testing is done with react-testing-library'
+  )
+})
+
+test('Render content button', async () => {
+    const mockHandler = vi.fn()
+    //debugger
+    const user = userEvent.setup()
+    const {container} =  render(<Note toggleImportance={mockHandler}/>)
+    const button = container.querySelector('#testID');
+    await user.click(button)
+    expect(mockHandler.mock.calls).toHaveLength(1)
+})
