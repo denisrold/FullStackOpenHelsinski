472a2c12fb675d7e56edaf37a680bf8bdf033ea3
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2024-06-17 12:05:20 -0300

    6.23

--- part6/6.3-6.19/redux-anecdotes/src/App.jsx
+++ part6/6.3-6.19/redux-anecdotes/src/App.jsx
@@ -2,10 +2,10 @@ import Notification from './components/Notification';
 import AnecdoteForm from './components/AnecdoteForm';
 import AnecdoteList from './components/AnecdoteList';
 import Filter from './components/Filter';
+
 import { useEffect } from 'react';
 import { initializeAnecdotes } from './reducers/anecdoteReducer';
 import { useDispatch } from 'react-redux';
-
 
 const App = () => {
   const dispatch = useDispatch();
