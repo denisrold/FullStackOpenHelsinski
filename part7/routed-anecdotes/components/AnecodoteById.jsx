17b8e190c391558a4cf633ba69d016e3a0488393
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2024-06-19 09:07:47 -0300

    react router dom

--- part7/routed-anecdotes/components/AnecodoteById.jsx
+++ part7/routed-anecdotes/components/AnecodoteById.jsx
@@ -1,7 +1,13 @@
+import { useParams } from "react-router-dom"
 const AnecdoteById = ({ anecdotes }) => {
+    const id = useParams().id
+    const anecdote = anecdotes.find(a => a.id=== Number(id))
     return (
       <div>
-        {anecdotes.id}
+        Este es una anecdota
+        <p>
+          {anecdote.content}
+        </p>
       </div>
       )
     }
