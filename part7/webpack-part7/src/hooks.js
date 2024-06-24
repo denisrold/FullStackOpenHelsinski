5f781785beb95528b94364549250009e01d5f396
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2024-06-24 06:18:47 -0300

    webpack ok

---
+++ part7/webpack-part7/src/hooks.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const useNotes = (url) => {
+  const [notes, setNotes] = useState([]);
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setNotes(response.data);
+    });
+  }, [url]);
+  return notes;
+};
+
+export default useNotes;
