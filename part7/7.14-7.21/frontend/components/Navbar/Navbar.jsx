985afbd85b9c29b6fcaf59c81ab3da00035c9a20
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2024-07-19 13:52:00 -0300

    css nav

--- part7/7.14-7.21/frontend/components/Navbar/Navbar.jsx
+++ part7/7.14-7.21/frontend/components/Navbar/Navbar.jsx
@@ -5,14 +5,28 @@ const Navbar = ({setUser, setLoadState}) => {
     return(
       <>
         <nav className="navbar_container">
-          <h1 className='Logo'>Blogguerse</h1>
-          <ul className='ul_navbar'>
-            <li><a>Home</a></li>
+          <div className='navFull'>
+            <h1 className='Logo'>Blogguerse</h1>
+            <ul className='ul_navbar'>
+              <li><a>Home</a></li>
+              <li><a>My blogs</a></li>
+              <li><a>Users</a></li>
+              <li><a>My info</a></li>
+            </ul>
+            <LogoutButton logoutStates={{setUser,setLoadState}}/>
+            </div>
+          <div className='media_cell'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 burguerButton" >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+          </svg>
+          </div>
+          <ul className='Cell_view'>
+          <li><a>Home</a></li>
             <li><a>My blogs</a></li>
             <li><a>Users</a></li>
             <li> <a>My info</a></li>
+            <LogoutButton logoutStates={{setUser,setLoadState}}/>
           </ul>
-          <LogoutButton logoutStates={{setUser,setLoadState}}/>
         </nav>
       </>
     )
