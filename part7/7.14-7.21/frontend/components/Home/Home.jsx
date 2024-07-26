84ac628d6ababd5bd3e2c1cedc870659592205b0
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2024-07-26 09:02:31 -0300

    swipper

--- part7/7.14-7.21/frontend/components/Home/Home.jsx
+++ part7/7.14-7.21/frontend/components/Home/Home.jsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Likes from '../Likes';
+import DisplayBlogs from '../Blog/DisplayBlogs';
 
 const Home = ({ user })=>{
     const navigate = useNavigate();
@@ -15,11 +16,13 @@ const Home = ({ user })=>{
    return ( user&&(
       <>
        <div name='userInfo' className='userInfo'>Hello {user.name}!</div>
+       {blogs&&<DisplayBlogs blogs={blogs}/>}
        <section className='bodyContainer'>
          {blogs&&(blogs.length?blogs.map((b,i) => (
-           <Blogs user={user} blog={b} key={i}>
-           <Likes blog={b} key={i}/>
-           </Blogs>
+            <Blogs user={user} blog={b} key={i}>
+            <Likes blog={b} key={i}/>
+            </Blogs>
+         
          ))
           :<h3 data-testid="noBlogs">No Blogs</h3>)}
         </section>
