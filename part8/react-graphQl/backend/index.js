7ba269857bb57b5c3879b275d1cb838208d658ad
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2024-08-12 08:05:51 -0300

    new back configuration

--- part8/react-graphQl/backend/index.js
+++ part8/react-graphQl/backend/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
+const { v1: uuid } = require("uuid");
 
 let persons = [
   {
@@ -25,17 +26,37 @@ let persons = [
 ];
 
 const typeDefs = `
+  type Address {
+    street: String!
+    city: String!
+  }
+
   type Person {
     name: String!
     phone: String
-    street: String!
-    city: String! 
+    address: Address!
     id: ID!
+  }
+
+  type Mutation {
+    addPerson(
+      name: String!
+      phone: String
+      street: String!
+      city: String!
+    ): Person
+
+    editNumber(name: String!, phone: String!): Person
+  }
+
+  enum YesNo {
+    YES
+    NO
   }
 
   type Query {
     personCount: Int!
-    allPersons: [Person!]!
+    allPersons(phone: YesNo): [Person!]!
     findPerson(name: String!): Person
   }
 `;
@@ -43,11 +64,53 @@ const typeDefs = `
 const resolvers = {
   Query: {
     personCount: () => persons.length,
-    allPersons: () => persons,
+    allPersons: (root, args) => {
+      if (!args.phone) {
+        return persons;
+      }
+      const byPhone = (person) =>
+        args.phone === "YES" ? person.phone : !person.phone;
+      return persons.filter(byPhone);
+    },
     findPerson: (root, args) => persons.find((p) => p.name === args.name),
   },
-};
 
+  Person: {
+    address: (root) => {
+      return {
+        street: root.street,
+        city: root.city,
+      };
+    },
+  },
+  Mutation: {
+    addPerson: (root, args) => {
+      if (persons.find((p) => p.name === args.name)) {
+        throw new GraphQLError("Name must be unique", {
+          extensions: {
+            code: "BAD_USER_INPUT",
+            invalidArgs: args.name,
+          },
+        });
+      }
+
+      const person = { ...args, id: uuid() };
+      persons = persons.concat(person);
+      return person;
+    },
+
+    editNumber: (root, args) => {
+      const person = persons.find((p) => p.name === args.name);
+      if (!person) {
+        return null;
+      }
+
+      const updatedPerson = { ...person, phone: args.phone };
+      persons = persons.map((p) => (p.name === args.name ? updatedPerson : p));
+      return updatedPerson;
+    },
+  },
+};
 const server = new ApolloServer({
   typeDefs,
   resolvers,
