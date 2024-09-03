d175c189185528eab2f990a8785e31b5577e9353
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2024-09-03 17:25:34 -0300

    added linted configuration

--- part9/typescript/calculator.ts
+++ part9/typescript/calculator.ts
@@ -1,5 +1,5 @@
 
-type Operation = 'multiply' | 'add' | 'divide';
+export type Operation = 'multiply' | 'add' | 'divide';
 type Result = string | number
 
 // const calculator = (a:number,b:number,op:Operation):Result=>{
@@ -11,28 +11,28 @@ type Result = string | number
 // }
 // };
 
-const calculator = (a: number, b: number, op: Operation) : number => {
+export const calculator = (a: number, b: number, op: Operation) : Result => {
   switch(op) {
      case 'multiply':
-       return a * b;
+       return Number(a) * Number(b);
      case 'divide':
       if (b === 0) throw new Error('Can\'t divide by 0!');
-      return a / b;
+      return Number(a) / Number(b);
      case 'add':
-       return a + b;
+       return Number(a) + Number(b);
      default:
       throw new Error('Operation is not multiply, add or divide!');
   }
- }
+ };
 
  
  try {
    console.log(calculator(1, 5,'divide'));
  } catch (error) {
-   let errorMessage = 'Something went wrong: '
+   let errorMessage = 'Something went wrong: ';
    if (error instanceof Error) {
      errorMessage += error.message;
    }
    console.log(errorMessage);
- }
+ };
  
--- part9/typescript/index.ts
+++ part9/typescript/index.ts
@@ -1,9 +1,35 @@
 import express from "express";
+import { calculator } from './calculator';
 
 const app = express();
+app.use(express.json());
 
 app.get('/ping', (_req, res) => {
   res.send('pong');
+});
+
+app.post('/calculate', (req, res) => {
+  try{
+    const { value1 , value2, op } = req.body;
+    if ( !value1 || isNaN(Number(value1)) ) {
+       throw new Error('Invalid value 1');
+    }
+    if ( !value2 || isNaN(Number(value2)) ) {
+      throw new Error('Invalid value 1');
+    }
+    if ( !op ){
+      throw new Error('Invalid value 1');
+    }
+    const result = calculator(value1, value2, op);
+    res.status(200).json({ result });
+  }
+  catch(err){
+    let errorMessage = "something was wrong: ";
+    if(err instanceof Error)
+      errorMessage += err.message;
+    res.status(400).json({error: errorMessage});
+  }
+ 
 });
 
 const PORT = 3003;
