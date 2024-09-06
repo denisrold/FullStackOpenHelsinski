5566da3c0628195478810b688e48b8a5b9a8dcc9
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2024-09-06 08:09:12 -0300

    added crud types

--- part9/diarios_de_vuelos/src/service/diaryService.ts
+++ part9/diarios_de_vuelos/src/service/diaryService.ts
@@ -1,5 +1,5 @@
 
-import { NonSensitiveDiaryEntry,DiaryEntry } from '../types';
+import { NonSensitiveDiaryEntry, DiaryEntry, NewDiaryEntry, Weather, Visibility } from '../types';
 import diaries from '../../data/diaries';
  
 const getEntries = (): DiaryEntry[] => {
@@ -16,11 +16,27 @@ const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
  };
 
  
-const addDiary = () => {
-  return null;
+const addDiary = ( date: string, weather: Weather, visibility: Visibility, comment?: string ) : NewDiaryEntry => {
+    const newDiaryEntry = { 
+      id: Math.max(...diaries.map(d => d.id)) + 1,
+      date,
+      weather,
+      visibility,
+      comment,
+    }
+    diaries.push(newDiaryEntry);
+    return newDiaryEntry;
 };
 
+const findById = (id: number): DiaryEntry | undefined => {
+
+  const entry = diaries.find(d => d.id === id);
+  return entry;
+ };
+ 
+
 export default {
+  findById,
   getEntries,
   addDiary,
   getNonSensitiveEntries
