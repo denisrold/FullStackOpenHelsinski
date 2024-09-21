2684fe94fda22c3e5e6ea5ee4288eeebbf60275f
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2024-09-21 16:03:19 -0300

    add select types

--- part9/9.20-9.21/patientor/src/components/AddEntryForm/SelectType.tsx
+++ part9/9.20-9.21/patientor/src/components/AddEntryForm/SelectType.tsx
@@ -6,9 +6,50 @@ interface SelectTypeProps {
 }
 const SelectType: React.FC<SelectTypeProps> = ({ newEntry, setNewEntry }) => {
   const handleOnSelect = (e : React.ChangeEvent<HTMLSelectElement>) => {
-    const target = e.target as HTMLInputElement | HTMLSelectElement;;
-      setNewEntry({...newEntry, [target.name]:target.value})
-   }  
+    const target = e.target as HTMLInputElement | HTMLSelectElement;
+    let newObjectBySelect  = {}
+
+    if (target.value === 'OccupationalHealthcare') {
+      newObjectBySelect = { 
+        description: '',
+        date: '',
+        type: target.value,
+        specialist: '',
+        diagnosisCodes: [],
+        employerName: '',
+        sickLeave: {
+          startDate: '',
+          endDate: '',
+        },
+      };
+    } 
+
+    else if (target.value === 'Hospital'){ 
+      newObjectBySelect = {
+        description: '',
+        date: '',
+        type: target.value,
+        specialist: '',
+        diagnosisCodes: [],
+        discharge: {
+          date: '',
+          criteria: '',
+        },
+      };
+    }
+    else if (target.value === 'HealthCheck'){ 
+      newObjectBySelect = {
+        description: '',
+        date: '',
+        type: target.value,
+        specialist: '',
+        diagnosisCodes: [],
+        healthCheckRating: 0,
+      };
+    }  
+    else { setNewEntry(newEntry) }
+    setNewEntry(newObjectBySelect as EntryWithoutId)
+  }
   return (
     <div className="inputsContainer">
       <label htmlFor="type">
