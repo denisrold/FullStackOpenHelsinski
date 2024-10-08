b0d4c9ae84222d5dc77fbc57b0282ff27525e24a
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2024-10-08 09:39:16 -0300

    starting test

--- part10/rate-repository-app/src/__tests__/exmple.js
+++ part10/rate-repository-app/src/__tests__/exmple.js
@@ -1,5 +1,12 @@
-describe("Example", () => {
-  it("works", () => {
-    expect(1).toBe(1);
-  });
+import { render } from "@testing-library/react-native";
+import { Button } from "react-native";
+// describe("Example", () => {
+//   it("works", () => {
+//     expect(1).toBe(1);
+//   });
+// });
+
+test('accessing queries using "render" result', () => {
+  const { getByRole } = render(<Button title="Start" onPress={() => {}} />);
+  getByRole("button", { name: "Start" });
 });
