be78e630873fdf192f63531ffb2e6d246229fbc4
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2024-10-14 09:27:26 -0300

    add test RepositoryList

--- part10/rate-repository-app/src/components/RepositoryList.jsx
+++ part10/rate-repository-app/src/components/RepositoryList.jsx
@@ -10,40 +10,41 @@ const styles = StyleSheet.create({
 
 const ItemSeparator = () => <View style={styles.separator} />;
 const renderItem = ({item}) => <RepositoryItems item={item}/>
+
+export const RepositoryListContainer = ({ repositories }) => {
+  const repositoryNodes = repositories
+    ? repositories.edges.map((edge) => edge.node)
+    : [];
+
+    if (!repositories) {
+      return <Text style={styles.emptyText}>Loading repositories...</Text>;
+    }
+  
+    if (repositoryNodes.length === 0) {
+      return <Text style={styles.emptyText}>No repositories found</Text>;
+    }
+    return (
+      <FlatList
+        data={repositoryNodes}
+        ItemSeparatorComponent={ItemSeparator}
+        renderItem={ renderItem }
+        keyExtractor={(item)=>item.id}
+      />
+    );
+};
+
 const RepositoryList = () => {
   const { repositories, loading, error } = useRepositories();
   if (loading) {
     return <ActivityIndicator size="large" />;
   }
-
   if (error) {
     return <Text style={styles.emptyText}>Error: {error.message}</Text>;
   }
-
   if (repositories.length === 0) {
     return <Text style={styles.emptyText}>No repositories found</Text>;
   }
-// Get the nodes from the edges array
-const repositoryNodes = repositories
-  ? repositories.edges.map(edge => edge.node)
-  : [];
-
-
-  if (!repositories) {
-    return <Text style={styles.emptyText}>Loading repositories...</Text>;
-  }
-
-  if (repositoryNodes.length === 0) {
-    return <Text style={styles.emptyText}>No repositories found</Text>;
-  }
-  return (
-    <FlatList
-      data={repositoryNodes}
-      ItemSeparatorComponent={ItemSeparator}
-      renderItem={ renderItem }
-      keyExtractor={(item)=>item.id}
-    />
-  );
+return <RepositoryListContainer repositories={repositories} />;
 };
 
 export default RepositoryList;
